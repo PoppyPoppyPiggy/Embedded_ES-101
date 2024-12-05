e302a3821ff29a4ba3a54875c63ccb64d57d06e8
minseong kim <129767606+PoppyPoppyPiggy@users.noreply.github.com>
2024-12-05 10:16:44 +0900

    Update test3.c

--- srcs/test/module/test3.c
+++ srcs/test/module/test3.c
@@ -10,48 +10,63 @@
 
 int sw[4] = {4, 17, 27, 22}; // 스위치 핀 번호
 int led[4] = {23, 24, 25, 1}; // LED 핀 번호
+
 static struct timer_list timer;
-static int blinking = 0; // LED 깜박임 상태
-static int flag = 0;     // LED 상태 토글
+static int mode = -1;       // 동작 모드 (-1: 없음, 0: 모든 LED 깜박임, 1: 순차적으로 LED 점등)
+static int led_index = 0;   // 순차적으로 LED를 점등하기 위한 인덱스
+static int flag = 0;        // LED 토글 상태
 
 // 타이머 콜백 함수
 static void timer_cb(struct timer_list *timer) {
     int i;
-    printk(KERN_INFO "Timer callback function!\n");
 
-    for (i = 0; i < 4; i++) {
-        gpio_set_value(led[i], flag ? LOW : HIGH);
+    if (mode == 0) {
+        printk(KERN_INFO "Timer callback for all LEDs blinking!\n");
+        for (i = 0; i < 4; i++) {
+            gpio_set_value(led[i], flag ? LOW : HIGH);
+        }
+        flag = !flag;
+    } else if (mode == 1) {
+        printk(KERN_INFO "Timer callback for sequential LED lighting! Index: %d\n", led_index);
+        for (i = 0; i < 4; i++) {
+            gpio_set_value(led[i], (i == led_index) ? HIGH : LOW);
+        }
+        led_index = (led_index + 1) % 4; // 다음 LED로 이동
     }
-    flag = !flag;
 
     mod_timer(timer, jiffies + HZ * 2);
 }
 
 // 스위치 인터럽트 핸들러
 irqreturn_t irq_handler(int irq, void *dev_id) {
-    int condition = -1;
     int i;
 
     switch (irq) {
-    case 60: // SW[0]: LED 깜박임 시작
+    case 60: // SW[0]: 모든 LED 깜박임 시작
         printk(KERN_INFO "SW1 interrupt occurred!\n");
-        if (!blinking) {
-            blinking = 1;
+        if (mode != 0) {
+            mode = 0;
+            flag = 0;
             mod_timer(&timer, jiffies + HZ * 2);
         }
         break;
 
-    case 61:
+    case 61: // SW[1]: 순차적으로 LED 점등 시작
         printk(KERN_INFO "SW2 interrupt occurred!\n");
+        if (mode != 1) {
+            mode = 1;
+            led_index = 0;
+            mod_timer(&timer, jiffies + HZ * 2);
+        }
         break;
 
     case 62:
         printk(KERN_INFO "SW3 interrupt occurred!\n");
         break;
 
-    case 63: // SW[3]: LED 모두 끄기
+    case 63: // SW[3]: 모든 LED 끄기 및 타이머 중지
         printk(KERN_INFO "SW4 interrupt occurred!\n");
-        blinking = 0;
+        mode = -1;
         del_timer(&timer);
         for (i = 0; i < 4; i++) {
             gpio_set_value(led[i], LOW);
